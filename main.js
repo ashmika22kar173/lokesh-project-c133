41bf05c3eba3138899b4e0ad06f205ab0af42fad
ashmika22kar173 <80112977+ashmika22kar173@users.noreply.github.com>
2021-10-19 07:08:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+   function bed_room(){
+       window.location="bedroom.html";
+   }
+
+   function TV_room(){
+    window.location="tvroom.html";
+}
+
+function besk(){
+    window.location="disk.html";
+}
+
+function bottles(){
+    window.location="bottle.html";
+}
+
+function fruit_basket(){
+window.location="fruit_basket.html";
+}
---
+++ tv.js
@@ -0,0 +1,57 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage("TV.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(350, 350);
+    canvas.position(490,230)
+    object = ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+}
+
+function draw() {
+    image(img, 0, 0, 350, 350);
+    if (status != "") {
+        object.detect(img, gotresult);
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Detected Objects";
+            document.getElementById("number").innerHTML = "Number of objects Detected =" + objects.length;
+
+            fill(r, g, b);
+            lokesh = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + lokesh + "%", objects[i].x , objects[i].y);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            
+        }
+
+    }
+}
+
+function modelloaded() {
+    console.log("model loaded");
+    status = true;
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+
+}
+
+function back(){
+    window.location="index.html";
+}
